d60d0dca005ca12facfb5cf3caf4f5375f8722f4
anonim1133 <anonim1133@wp.pl>
2015-02-27 13:19:04 +0100

    Poprawione wyświetlanie tytułu, zgodnie z obsługiwanym tagiem.

--- templates/head.php
+++ templates/head.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8" />
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="description" content="description">
-	<title>#rowerowyrownik</title>
+	<title><?php include_once 'settings.php'; $s = new Settings(); echo '#'.$s->getTag() ?></title>
 	<link rel="stylesheet" media="screen" href="style.css" />
 	<!--[if IE]>
 	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
